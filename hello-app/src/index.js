176de4a9152760c7cc9243c4a0bbef760f04399d
Tanmay Khedekar <tanmay@osseed.com>
2018-01-02 13:29:06 +0530

    Hello to ReactJS app.

---
+++ hello-app/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+
+ReactDOM.render(
+  <h1>Say Hello to my first ReactJS app...!!</h1>,
+  document.getElementById('root')
+);
